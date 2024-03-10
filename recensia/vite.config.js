11e46078a2487c44306df533eddce67ba5418f0c
kashapov <kashapovandrey@gmail.com>
2024-03-10 10:01:44 +0100

    deploy

--- recensia/vite.config.js
+++ recensia/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/recensia/', // Specify the base path here
   plugins: [react()],
 })
