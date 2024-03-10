e2ce1f7a26108b393efa052db7c8e5d2727ce50d
kashapov <kashapovandrey@gmail.com>
2024-03-10 10:03:20 +0100

    add endpoint

--- recensia/src/App.jsx
+++ recensia/src/App.jsx
@@ -14,7 +14,8 @@ function App() {
     setSearchQuery(value);
     setLoading(true);
     // Make an API request to fetch search results
-    axios.get(`https://your-api-endpoint.com/search?q=${value}`)
+    // https://apps.apple.com/ua/app/pixelmator-pro/id1289583905?mt=12
+      axios.get(`https://recensia-2fd235ed82fc.herokuapp.com/analyse/raw/?app_store_url=${value}`)
       .then(response => {
         setSearchResults(response.data);
         setLoading(false);
